aecce7b862d71877e0baba0ba3241e9621198388
Ahmed Ghalab <0xcodezero@gmail.com>
2014-08-21 18:41:14 +0300

    Single view Protrait NuWeDevApp for Dynamic linking to the framework

---
+++ NuWeDevApp/NuWeDevApp/NWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NWAppDelegate.h
+//  NuWeDevApp
+//
+//  Created by Ahmed Ghalab on 8/21/14.
+//  Copyright (c) 2014 Nutribu Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NuWeDevApp/NuWeDevApp/NWViewController.h
@@ -0,0 +1,13 @@
+//
+//  NWViewController.h
+//  NuWeDevApp
+//
+//  Created by Ahmed Ghalab on 8/21/14.
+//  Copyright (c) 2014 Nutribu Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NWViewController : UIViewController
+
+@end
